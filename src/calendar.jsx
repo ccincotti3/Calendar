db5df18aec961e16dc4d92633ff008393f16bf2f
Carmen <ccincotti3@gmail.com>
2017-08-29 16:34:35 -0400

    Parse today's date

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 class Calendar extends React.Component {
   constructor(props) {
+    super(props);
+    this.state = {month: null, day: null, year: null}
+  }
 
+  componentDidMount() {
+    let date = new Date();
+    let month = date.getMonth();
+    let day = date.getDay();
+    let year = date.getYear();
   }
 
   render() {
-    return "Calendar connection"
+    return <p>"Calendar"</p>
   }
 }
 
