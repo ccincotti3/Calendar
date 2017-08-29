4af439280bb05f542d0330131dc6765c60407439
Carmen <ccincotti3@gmail.com>
2017-08-29 16:21:43 -0400

    Initialize files

---
+++ src/calendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Calendar extends React.Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return "Calendar connection"
+  }
+}
+
+export default Calendar
