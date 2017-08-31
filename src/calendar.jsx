90689d7ee289140f052d1b9736eea29fdd5028fd
Carmen <ccincotti3@gmail.com>
2017-08-31 13:13:15 -0400

    Add basic modal

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -3,9 +3,10 @@ import './calendar.css'
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {dateObj: null, month: null, day: null, year: null}
+    this.state = {dateObj: null, month: null, day: null, year: null, modal: false}
     this.populateCalendar = this.populateCalendar.bind(this);
     this.changeDir = this.changeDir.bind(this);
+    this.goModal = this.goModal.bind(this);
   }
 
   componentDidMount() {
@@ -15,6 +16,22 @@ class Calendar extends React.Component {
     let year = date.getFullYear();
 
     this.setState({dateObj:date, month: month, day: day, year: year});
+  }
+
+  goModal() {
+    this.setState({modal: !this.state.modal})
+    console.log('click')
+  }
+
+  buildModal() {
+    return (
+      <div className="modal">
+        <div className="modal-content">
+           <span onClick={this.goModal} className="close">&times;</span>
+          <h1>Hi</h1>
+        </div>
+      </div>
+    )
   }
 
   parseMonth(num) {
@@ -68,25 +85,38 @@ class Calendar extends React.Component {
       weeks.push(thisWeek);
     }
     const mapWeeks = weeks.map((arr, i) => {
-      return <Week days={arr} key={i} />
+      return <Week modalFn={this.goModal} days={arr} key={i} />
     });
     return mapWeeks;
   }
 
   changeDir(dir) {
     if(dir === 'left') {
-      this.setState({month: (this.state.month - 1) % 12});
+      if(this.state.month === 0) {
+        this.setState({year: this.state.year - 1});
+        this.setState({month: 11});
+      } else {
+        this.setState({month: (this.state.month - 1)});
+      }
     } else {
-      this.setState({month: (this.state.month + 1) % 12});
+      if(this.state.month === 11) {
+        this.setState({year: this.state.year + 1});
+        this.setState({month: 0});
+      } else {
+        this.setState({month: (this.state.month + 1)});
+      }
     }
     return;
   }
 
   render() {
+    const modal = this.state.modal ? this.buildModal() : ""
+
     const monthName = this.parseMonth(this.state.month)
     const days = this.populateCalendar(this.state.month, this.state.year);
     return (
       <div className="calendar">
+        {modal}
         <div className="calendar-header">
           <span>
             <button onClick={() => this.changeDir('left')}>Left</button>
@@ -112,7 +142,7 @@ class Week extends React.Component {
   render() {
     const dayBox = this.props.days.map((day, i) => {
       return (
-          <td className="box" key={i}>
+          <td className="box" onClick={this.props.modalFn} key={i}>
             {day}
           </td>
     )
